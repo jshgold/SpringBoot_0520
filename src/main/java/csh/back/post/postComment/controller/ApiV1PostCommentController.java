9692d47ff173071609421e0ba6f16c8842baa4da
devho <jshgold93@gmail.com>
2026-05-21 12:36:00 +0900

    댓글 수정 기능 구현

--- src/main/java/csh/back/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/csh/back/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,6 +5,7 @@ import csh.back.post.post.entity.Post;
 import csh.back.post.post.service.PostService;
 import csh.back.post.postComment.dto.PostCommentDto;
 import csh.back.post.postComment.entity.PostComment;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -62,5 +63,19 @@ public class ApiV1PostCommentController {
                 "%d번 댓글이 삭제되었습니다.".formatted(postComment.getId()), commentDto);
 
         return rsData;
+    }
+
+    @PutMapping("/edit")
+    @Transactional
+    public RsData<PostCommentDto> edit(@RequestBody CommentReqBody req) {
+        Post post = postService.findById(req.postId).get();
+        PostComment comment = post.findCommentById(req.id).get();
+        postService.modifyComment(comment,req.comment);
+        PostCommentDto commentDto = new PostCommentDto(comment);
+        return new RsData<>("200-1","성공햇어?", commentDto);
+    }
+
+    record CommentReqBody(@NotBlank int postId, @NotBlank int id, @NotBlank String comment) {
+
     }
 }
